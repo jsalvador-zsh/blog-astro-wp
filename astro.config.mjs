85c08a2b63eacedef0f8cc2b470fbbead4a1b816
JuanSalvadorT <juanstdev@gmail.com>
2025-05-15 09:49:18 -0500

    [ACT] Cambio en estilos de la página

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   // output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [icon()]
 });
--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -40,6 +40,14 @@ export const getLatestPosts = async ({ perPage = 10 }: { perPage?: number } = {}
             slug
         } = post
         const featuredImage = post._embedded['wp:featuredmedia'][0].source_url
+
+        const tags = post._embedded['wp:term']?.[1]?.map((tag: any) => ({
+            id: tag.id,
+            name: tag.name,
+            slug: tag.slug,
+            link: tag.link
+        })) || []
+
         const formattedDate = new Intl.DateTimeFormat('es-ES', {
             day: '2-digit',
             month: 'long',
@@ -49,34 +57,42 @@ export const getLatestPosts = async ({ perPage = 10 }: { perPage?: number } = {}
             hour12: true
         }).format(new Date(date))
 
-        return { title, excerpt, content, date: formattedDate, slug, featuredImage }
+        return { title, excerpt, content, date: formattedDate, slug, featuredImage, tags }
     })
     return posts
 }
 
 export const getPostInfo = async (slug: string) => {
-    const response = await fetch(`${apiURL}/posts?slug=${slug}`)
+    const response = await fetch(`${apiURL}/posts?slug=${slug}&_embed=1`);
     if (!response.ok) {
-        throw new Error(`Error fetching page info: ${response.statusText}`)
+        throw new Error(`Error fetching post info: ${response.statusText}`);
     }
 
-    const [data] = await response.json()
+    const [data] = await response.json();
+
     const {
         title: { rendered: title },
         content: { rendered: content },
         date,
-        excerpt: { rendered: excerpt }
-    } = data
+        excerpt: { rendered: excerpt },
+    } = data;
 
-    // Formatear la fecha
+    const tags = data._embedded?.['wp:term']?.[1]?.map((tag: any) => ({
+        id: tag.id,
+        name: tag.name,
+        slug: tag.slug,
+        link: tag.link,
+    })) || [];
+
     const formattedDate = new Intl.DateTimeFormat('es-ES', {
         day: '2-digit',
         month: 'long',
         year: 'numeric',
         hour: '2-digit',
         minute: '2-digit',
-        hour12: true
-    }).format(new Date(date))
+        hour12: true,
+    }).format(new Date(date));
 
-    return { title, content, date: formattedDate, excerpt }
-}
+    return { title, content, date: formattedDate, excerpt, tags };
+};
+
